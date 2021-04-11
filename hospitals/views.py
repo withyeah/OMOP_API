42591ed0530ac0e2c7189b9b2d6043815142b4a0
Alex Gim <yerang.dev@gmail.com>
2021-04-11 21:08:36 +0900

    Problem 1-2

--- hospitals/views.py
+++ hospitals/views.py
@@ -46,3 +46,39 @@ def patients_list(request):
     }
     return Response(data)
 
+
+@api_view(['GET'])
+def visits_list(request):
+    queryset = get_list_or_404(VisitOccurrence)
+
+    visit_type_data = {}
+    visit_gender_data = {}
+    visit_race_data = {}
+    visit_ethnicity_data = {}
+
+    for visit in queryset.iterator():
+        visit_type = Concept.objects.get(concept_id=visit.visit_concept_id).concept_name
+        visit_type_data[visit_type] = visit_type_data.get(visit_type, 0) + 1
+
+        gender = Person.objects.get(person_id=visit.person_id).gender_concept_id
+        visit_gender = Concept.objects.get(concept_id=gender).concept_name
+        visit_gender_data[visit_gender] = visit_gender_data.get(visit_gender, 0) + 1
+
+        race = Person.objects.get(person_id=visit.person_id).race_concept_id
+        visit_race = Concept.objects.get(concept_id=race).concept_name
+        visit_race_data[visit_race] = visit_race_data.get(visit_race, 0) + 1
+
+        ethnicity = Person.objects.get(person_id=visit.person_id).ethnicity_concept_id
+        visit_ethnicity = Concept.objects.get(concept_id=ethnicity).concept_name
+        visit_ethnicity_data[visit_ethnicity] = (
+            visit_ethnicity_data.get(visit_ethnicity, 0) + 1
+        )
+
+    data = {
+        'visit_type_count': visit_type_data,
+        'visit_gender_count': visit_gender_data,
+        'visit_race_count': visit_race_data,
+        'visit_ethnicity_count': visit_ethnicity_data,
+    }
+
+    return Response(data)
